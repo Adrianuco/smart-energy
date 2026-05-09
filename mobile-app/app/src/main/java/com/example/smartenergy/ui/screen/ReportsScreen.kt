99d63f898222ba031c80834d0ba5b870d72a2977
denisuam3 <dgmolinaa@uamv.edu.ni>
2026-05-09 10:46:41 -0600

    Agregar Scaffold y TopBar a pantalla de reportes

--- mobile-app/app/src/main/java/com/example/smartenergy/ui/screen/ReportsScreen.kt
+++ mobile-app/app/src/main/java/com/example/smartenergy/ui/screen/ReportsScreen.kt
@@ -1,9 +1,31 @@
 package com.example.smartenergy.ui.screen
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ReportsScreen() {
-    Text(text = "Pantalla de Reportes en desarrollo")
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Reportes")
+                }
+            )
+        }
+    ) { paddingValues ->
+
+        Text(
+            text = "Pantalla de Reportes  ",
+            modifier = Modifier.padding(paddingValues)
+        )
+
+    }
 }
