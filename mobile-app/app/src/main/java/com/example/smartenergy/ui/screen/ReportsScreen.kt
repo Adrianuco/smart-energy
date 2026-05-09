abea5f1d49c118b520f0f61489bd14347105cc76
denisuam3 <dgmolinaa@uamv.edu.ni>
2026-05-09 10:15:48 -0600

    Creación de ReportsScreen.kt en ui/screen

---
+++ mobile-app/app/src/main/java/com/example/smartenergy/ui/screen/ReportsScreen.kt
@@ -0,0 +1,9 @@
+package com.example.smartenergy.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+
+@Composable
+fun ReportsScreen() {
+    Text(text = "Pantalla de Reportes en desarrollo")
+}
