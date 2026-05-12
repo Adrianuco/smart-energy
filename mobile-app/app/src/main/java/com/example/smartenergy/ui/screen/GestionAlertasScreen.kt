8b4d259534761979d017346c60fbeb7754d51722
yvgarcia <yvgarcia@uamv.edu.ni>
2026-05-11 23:00:57 -0600

    Agregada estructura de datos para alertas

--- mobile-app/app/src/main/java/com/example/smartenergy/ui/screen/GestionAlertasScreen.kt
+++ mobile-app/app/src/main/java/com/example/smartenergy/ui/screen/GestionAlertasScreen.kt
@@ -3,6 +3,14 @@ package com.example.smartenergy.ui.screen
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
+data class Alerta(
+    val aula: String,
+    val edificio: String,
+    val tipo: String,
+    val hora: String,
+    val estado: String
+)
+
 @Composable
 fun GestionAlertasScreen() {
     Text("Gestión de Alertas")
