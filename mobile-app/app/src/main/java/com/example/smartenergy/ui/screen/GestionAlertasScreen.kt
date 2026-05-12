709c115915e09a43221d918fd0e6bc9b85dc9fc3
yvgarcia <yvgarcia@uamv.edu.ni>
2026-05-11 23:01:26 -0600

    Añadida lista simulada de alertas para edificios

--- mobile-app/app/src/main/java/com/example/smartenergy/ui/screen/GestionAlertasScreen.kt
+++ mobile-app/app/src/main/java/com/example/smartenergy/ui/screen/GestionAlertasScreen.kt
@@ -11,6 +11,14 @@ data class Alerta(
     val estado: String
 )
 
+val listaAlertas = listOf(
+    Alerta("Aula B-104", "Edificio B", "Apagar AC", "2:30 PM", "Pendiente"),
+    Alerta("Aula C-202", "Edificio C", "Revisar consumo", "3:15 PM", "Advertencia"),
+    Alerta("Aula E-101", "Edificio E", "Apagar AC", "5:00 PM", "Pendiente"),
+    Alerta("Aula A-104", "Edificio A", "Todo correcto", "1:00 PM", "Resuelta"),
+    Alerta("Aula D-202", "Edificio D", "Encender AC", "7:45 AM", "Pendiente")
+)
+
 @Composable
 fun GestionAlertasScreen() {
     Text("Gestión de Alertas")
