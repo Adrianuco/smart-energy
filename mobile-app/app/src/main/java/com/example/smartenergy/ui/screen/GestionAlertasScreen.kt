3f98572f8424f97871fb4cbdb9b2bd8cad654a22
yvgarcia <yvgarcia@uamv.edu.ni>
2026-05-11 23:02:19 -0600

    Implementada estructura visual principal de Gestión de Alertas

--- mobile-app/app/src/main/java/com/example/smartenergy/ui/screen/GestionAlertasScreen.kt
+++ mobile-app/app/src/main/java/com/example/smartenergy/ui/screen/GestionAlertasScreen.kt
@@ -1,7 +1,22 @@
 package com.example.smartenergy.ui.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 data class Alerta(
     val aula: String,
@@ -21,5 +36,45 @@ val listaAlertas = listOf(
 
 @Composable
 fun GestionAlertasScreen() {
-    Text("Gestión de Alertas")
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Gestión de Alertas")
+                }
+            )
+        }
+    ) { paddingValues ->
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(horizontal = 16.dp, vertical = 12.dp)
+        ) {
+
+            Text(
+                text = "Alertas activas y registradas",
+                fontSize = 24.sp,
+                style = MaterialTheme.typography.titleMedium
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            LazyColumn(
+                contentPadding = PaddingValues(bottom = 16.dp),
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+
+                items(listaAlertas) { alerta ->
+
+                    Text(
+                        text = "${alerta.aula} | ${alerta.edificio} - ${alerta.estado}",
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+            }
+        }
+    }
 }
